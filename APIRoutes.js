04e26feeb4354b4559274fcc976727c5f0eafe6e
James Kan <james0077721@hotmail.com>
2020-10-07 00:03:37 -0700

    default route

--- APIRoutes.js
+++ APIRoutes.js
@@ -1,10 +1,15 @@
+module.exports = function (app) {
+  let youtubeLearning = require("./APIContoller");
+  app.route("/storeInitialReview").post(youtubeLearning.addNewSession);
+  app
+    .route("/storeEndSessionInfo/:videoSessionId")
+    .post(youtubeLearning.addEndSession);
 
-module.exports = function(app) {
-	let youtubeLearning = require('./APIContoller');
-	app.route('/storeInitialReview').post(youtubeLearning.addNewSession);
-	app.route('/storeEndSessionInfo/:videoSessionId').post(youtubeLearning.addEndSession);
+  // Unimplemented Ports
+  let dataRetrevial = require("./UserDataOperations");
+  app.route("/getUserSessions/:userId").get(dataRetrevial.GetUserSessions);
 
-	// Unimplemented Ports
-	let dataRetrevial = require('./UserDataOperations')
-	app.route("/getUserSessions/:userId").get(dataRetrevial.GetUserSessions);
+  app.route("/").get(() => {
+    return "hello!";
+  });
 };
